e285681c6e89971993c86e5f3375947789344b84
KinTat Ho <kintath@gmail.com>
2023-06-24 21:38:17 +0800

    calories screen same theme now heheheh

--- screens/CaloriesScreen.js
+++ screens/CaloriesScreen.js
@@ -18,7 +18,7 @@ const CaloriesScreen = ({navigation}) => {
   };
 
   return (
-    <View style={styles.screen}>
+    <View style={styles.container}>
       <Button
         title='Back to profile'
         onPress={backButtonHandler}
@@ -38,8 +38,8 @@ const CaloriesScreen = ({navigation}) => {
         data={caloriesData}
         renderItem={itemData => (
           <View style={styles.listItem}>
-            <Text>{itemData.item.date}</Text>
-            <Text>{itemData.item.value} Calories</Text>
+            <Text style={styles.itemText}>{itemData.item.date}</Text>
+            <Text style={styles.itemText}>{itemData.item.value} Calories</Text>
           </View>
         )}
       />
@@ -48,34 +48,55 @@ const CaloriesScreen = ({navigation}) => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50
+  container: {
+    flex: 1,
+    backgroundColor: '#010202',
+    padding: 20,
   },
   title: {
-    fontSize: 18,
+    fontSize: 24,
+    fontWeight: 'bold',
     marginBottom: 20,
-    textAlign: 'center'
+    textAlign: 'center',
+    color: '#FFF',
   },
   inputContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 20
+    marginBottom: 20,
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: '#333',
   },
   input: {
     width: '80%',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10
-  },
-  listItem: {
-    borderColor: 'black',
+    borderColor: '#87CEFA',
     borderWidth: 1,
     padding: 10,
-    marginBottom: 10,
+    color: '#FFF',
+  },
+  listItem: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: '#333',
+    marginVertical: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+  itemText: {
+    fontSize: 14,
+    color: '#87CEFA',
+  },
 });
 
 export default CaloriesScreen;
