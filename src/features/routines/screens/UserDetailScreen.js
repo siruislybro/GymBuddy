e7fd385300f08e631e39c13955e23abb92f759eb
siruislybro <tansirui2@gmail.com>
2023-07-02 17:59:35 +0800

    improved openai API prompt

--- src/features/routines/screens/UserDetailScreen.js
+++ src/features/routines/screens/UserDetailScreen.js
@@ -14,7 +14,8 @@ const UserDetailScreen = ({ navigation, route }) => {
   // Function to handle when the submit button is pressed
   const handleSubmit = async () => {
     // Construct the payload for the OpenAI API
-    const prompt = `My height is ${height}, my weight is ${weight}, I am able to workout ${numWorkouts} times per week, and my goal is to ${goal}. Generate a workout plan for me that includes weights.`;
+    const prompt = `My height is ${height} cm, my weight is ${weight}kg and my goal is to ${goal}. Generate ${numWorkouts} weightlifting workout plans for me.`;
+    console.log(prompt)
     const maxTokens = 500; // Set the desired max number of tokens in the response
     const API_KEY = "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";
 
