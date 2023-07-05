1ca30c88607d0dfb64d5d51b1581b664cc05e7ae
KinTat Ho <kintath@gmail.com>
2023-07-06 01:18:44 +0800

    now keeps track of calories left + current calories

--- src/features/calorieTracker/screens/CaloriesScreen.js
+++ src/features/calorieTracker/screens/CaloriesScreen.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, ActivityIndicator, TextInput, StyleSheet, Button, ScrollView } from 'react-native';
+import { View, Text, FlatList, ActivityIndicator, TextInput, StyleSheet, Button, ScrollView, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 import BackButton from '../../../components/BackButton';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc, updateDoc, Timestamp } from 'firebase/firestore';
 import { db, auth } from '../../../../firebase';
 
 const CaloriesScreen = ( {navigation} ) => {
@@ -11,12 +11,56 @@ const CaloriesScreen = ( {navigation} ) => {
   const [recipes, setRecipes] = useState([]);
   const [recommendedCalories, setRecommendedCalories] = useState(null);
   const [manualCalories, setManualCalories] = useState('');
+  const [currentCalories, setCurrentCalories] = useState(0);
+  const [lastUpdated, setLastUpdated] = useState(null);
   const user = auth.currentUser;
+ 
 
   const knowYourCalories = () => {
     navigation.navigate("RecommendedCalories")
   }
+  
+  
+// Fetch recommended and current calories from Firestore
+useEffect(() => {
+  const fetchCaloriesData = async () => {
+    try {
+      const docSnap = await getDoc(doc(db, 'users', user.uid, 'nutrition', 'calories'));
+      if (docSnap.exists()) {
+        const data = docSnap.data();
+        setRecommendedCalories(data.recommended);
 
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        const todayTimestamp = Timestamp.fromDate(today);
+        console.log(todayTimestamp);
+
+        if (data.lastUpdated && data.lastUpdated.toDate() < today) {
+          setCurrentCalories(0);
+          setLastUpdated(todayTimestamp);
+          await updateDoc(doc(db, 'users', user.uid, 'nutrition', 'calories'), {
+            current: 0,
+            lastUpdated: todayTimestamp
+          });
+        } else if (data.current) {
+          setCurrentCalories(data.current);
+          setLastUpdated(data.lastUpdated ? data.lastUpdated.toDate() : null);
+        } else {
+          setCurrentCalories(0);
+          setLastUpdated(null);
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching calories data:", error);
+    }
+  }
+
+  fetchCaloriesData();
+}, []);
+
+
+
+  
     // Fetch recommended calories from Firestore
     useEffect(() => {
       const fetchRecommendedCalories = async () => {
@@ -36,12 +80,24 @@ const CaloriesScreen = ( {navigation} ) => {
     const handleManualSubmit = async () => {
       try {
         await setDoc(doc(db, 'users', user.uid, 'nutrition', 'calories'), {
-          recommended: manualCalories
+          recommended: manualCalories,
+          current: 0,
+          lastUpdated: new Date()
         });
         setRecommendedCalories(manualCalories);
+        setCurrentCalories(0);
+        setLastUpdated(new Date());
       } catch (error) {
         console.error("Error setting recommended calories:", error);
       }
+    }
+
+    const handleItemPress = async (calories) => {
+      const newCurrent = currentCalories + calories;
+      setCurrentCalories(newCurrent);
+      await updateDoc(doc(db, 'users', user.uid, 'nutrition', 'calories'), {
+        current: newCurrent
+      });
     }
 
   const fetchRecipes = async () => {
@@ -93,7 +149,11 @@ const CaloriesScreen = ( {navigation} ) => {
         <Button title="Search" onPress={onSearch} />
       </View>
       {recommendedCalories ? (
-        <Text style={styles.title}>Recommended Daily Calories: {recommendedCalories}</Text>
+        <View>
+          <Text style={styles.title}>Recommended Daily Calories: {recommendedCalories}</Text>
+          <Text style={styles.title}>Current Calories: {currentCalories}</Text>
+          <Text style={styles.title}>Calories Left: {recommendedCalories - currentCalories}</Text>
+        </View>
       ) : (
         <View>
           <Text>No recommended calories set.</Text>
@@ -113,10 +173,10 @@ const CaloriesScreen = ( {navigation} ) => {
           data={recipes}
           keyExtractor={(item) => item.name.toString()}
           renderItem={({ item }) => (
-            <View style={styles.itemContainer}>
+            <TouchableOpacity style={styles.itemContainer} onPress={() => handleItemPress(item.calories)}>
               <Text style={styles.itemName}>{item.name}</Text>
               <Text style={styles.itemCalories}>Calories: {item.calories}</Text>
-            </View>
+            </TouchableOpacity>
           )}
         />
       ) : (
