93ead6f7f7467915b5779a105e0cf060c18240d3
siruislybro <tansirui2@gmail.com>
2023-07-15 02:44:13 +0800

    Incomplete - Saving Routine from openai

--- src/features/routines/screens/RecommendedRoutineScreen.js
+++ src/features/routines/screens/RecommendedRoutineScreen.js
@@ -1,10 +1,71 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text, View, TouchableOpacity, Alert } from 'react-native';
 import BackButton from '../../../components/BackButton';
+import { db, auth } from '../../../../firebase';
+import { useNavigation } from '@react-navigation/native';
 
 const RecommendedRoutineScreen = ({ route }) => {
-  // Extract the recommendedRoutine data from the route parameters
+  const navigation = useNavigation();
+  const user = auth.currentUser;
   const { recommendedRoutine } = route.params;
+  const exercisePattern = /^• (.+): (\d+) sets of (\d+) reps$/; // Assumes exercise lines start with a bullet point (•) and follow the format "Exercise Name: Sets sets of Reps reps"
+  console.log(exercisePattern)
+  const workoutPlans = [];
+  const lines = recommendedRoutine.split('\n');
+  let currentPlan = null;
+  lines.forEach((line) => {
+    line = line.trim();
+    
+     // Check if the line starts with "Workout Plan" to identify new workout plans
+    if (line.startsWith('Workout Plan')) {
+    // Create a new workout plan object
+    currentPlan = {
+      planName: line,
+      exercises: [],
+    };
+    workoutPlans.push(currentPlan);
+  }
+  
+  // Check if the line matches the exercise pattern
+  const exerciseMatch = line.match(exercisePattern);
+  if (exerciseMatch) {
+    // Extract exercise details
+    const exerciseName = exerciseMatch[1];
+    const sets = parseInt(exerciseMatch[2]);
+    const reps = parseInt(exerciseMatch[3]);
+
+    if (currentPlan) {
+      // Push the exercise to the current workout plan
+      currentPlan.exercises.push({ name: exerciseName, sets:[{weight: '', reps: reps}] });
+    }
+    }
+  });
+  console.log(currentPlan)
+
+  const saveWorkoutPlans = () => {
+    workoutPlans.forEach((plan, index) => {
+      const workoutPlanRef = db
+        .collection('users')
+        .doc(user.uid)
+        .collection('workoutPlans')
+        .doc(`Workout Plan ${index + 1}`);
+      
+      workoutPlanRef
+        .set(plan)
+        .then(() => {
+          console.log(`Workout plan ${index + 1} saved successfully!`);
+        })
+        .catch((error) => {
+          console.error(`Error saving workout plan ${index + 1}:`, error);
+        });
+    });
+    Alert.alert('Success', 'Workout plans saved successfully!', [{
+        text: 'OK',
+        onPress: () => navigation.navigate('WorkoutPlans'),
+      },
+    ]);
+  
+  };
 
   return (
     <ScrollView style={styles.container}>
@@ -13,6 +74,9 @@ const RecommendedRoutineScreen = ({ route }) => {
       </View>
       <Text style={styles.heading}>Recommended Routine</Text>
       <Text style={styles.routine}>{recommendedRoutine}</Text>
+      <TouchableOpacity style={styles.button} onPress={saveWorkoutPlans}>
+        <Text style={styles.buttonText}>Save Workouts</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 };
@@ -39,6 +103,17 @@ const styles = {
   },
   routine: {
     color: 'white'
+  },
+  button: {
+    backgroundColor: '#0484fb',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 18,
   },
 };
 
